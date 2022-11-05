5faca65305b7deaae367029daff81a86556c1df8
Luan Roger A. Ferreira <105799447+luanrogerr@users.noreply.github.com>
2022-11-05 00:32:43 -0300

    Add files via upload

---
+++ menu-admin/usuarioexcluir.php
@@ -0,0 +1,24 @@
+ <?php
+session_start(); 
+ 
+//Faz a leitura do dado passado pelo link.
+$campoid = filter_input(INPUT_GET, 'id');
+ 
+//Faz a conexão com o BD.
+require 'conexaoadmin.php';
+
+// Apagar da tabela usuários o registro com o id
+$sql = "DELETE FROM usuarios WHERE id=$campoid";
+
+//Executa o sql e faz tratamento de erro.
+if ($conn->query($sql) === TRUE) {
+  
+   header('Location: ../menu-admin/controlarusuarios.php?pag=1'); //Redireciona para o controle  
+} else {
+  echo "Erro: " . $conn->error;
+}
+
+//Fecha a conexão.
+$conn->close();
+
+?> 
